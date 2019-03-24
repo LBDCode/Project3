0134052fcbf63e68e8a02defa8c83b8fbfb66e36
DPadillaNYG <DPadilla.NYG@gmail.com>
2019-03-23 22:45:49 -0400

    Sidebar Options & Navbar CSS Fixed

---
+++ client/src/components/Navbar/Sidebar/AllergyOptions.js
@@ -0,0 +1,91 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  formControl: {
+    margin: theme.spacing.unit * 3
+  }
+});
+
+class CheckboxesGroup extends React.Component {
+  state = {
+    dairy_free: true,
+    gluten_free: false,
+    peanut_free: false,
+    shellfish_free: false
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { dairy_free, gluten_free, peanut_free, shellfish_free } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">Food Allergies</FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={dairy_free}
+                  onChange={this.handleChange("dairy_free")}
+                  value="dairy_free"
+                />
+              }
+              label="Dairy-free"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={gluten_free}
+                  onChange={this.handleChange("gluten_free")}
+                  value="gluten_free"
+                />
+              }
+              label="Gluten-free"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={peanut_free}
+                  onChange={this.handleChange("peanut_free")}
+                  value="peanut_free"
+                />
+              }
+              label="Peanut-free"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={shellfish_free}
+                  onChange={this.handleChange("shellfish_free")}
+                  value="shellfish_free"
+                />
+              }
+              label="Shellfish-free"
+            />
+          </FormGroup>
+        </FormControl>
+      </div>
+    );
+  }
+}
+
+CheckboxesGroup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(CheckboxesGroup);
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,59 +1,48 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import RecipeSearchIcon from "@material-ui/icons/ImageSearch";
 import DashboardIcon from "@material-ui/icons/TableChart";
 import { Link } from "react-router-dom";
-import UserOptions from "./UserOptions";
+import UserOptions from "./Sidebar/UserOptions";
 import "./style.css";
 
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-};
-
 function ButtonAppBar(props) {
-  const { classes } = props;
   return (
-    <div className={classes.root}>
-      <Button color="inherit">Login</Button>
-      <Button color="inherit">Sign Up</Button>
+    <div>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className="navFlex">
           <Tabs
-            className="navtext"
+            className="navText"
             variant="scrollable"
             // scrollButtons="on"
             indicatorColor="primary"
             textColor="primary"
           >
-            <Link to={"/search"} className="navtext">
+            <Link to={"/search"} className="navText">
               {" "}
               <Tab
                 label="Search"
                 icon={<RecipeSearchIcon className="navcons" />}
               />
             </Link>
-            <Link to={"/dashboard"} className="navtext">
+            <Link to={"/dashboard"} className="navText">
               <Tab
                 label="Dashboard"
                 icon={<DashboardIcon className="navcons" />}
               />
             </Link>
-            <UserOptions navtexts="navtext" navcon="navcons" />
+            <UserOptions
+              navtext="navText"
+              navButton="navButton"
+              navcon="navcons"
+              sidebarLoginButtons="sidebarLoginButtons"
+              sidebarLoginText="sidebarLoginText"
+              sidebarSaveButton="sidebarSaveButton"
+              sidebarSavePlacement="sidebarSavePlacement"
+            />
           </Tabs>
         </Toolbar>
       </AppBar>
@@ -61,8 +50,4 @@ function ButtonAppBar(props) {
   );
 }
 
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(ButtonAppBar);
+export default ButtonAppBar;
