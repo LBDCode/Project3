4b8924172e36863c58b111f8f55bd6569a9ee1b7
siaustin <sara.austin16@gmail.com>
2019-03-30 10:13:02 -0400

    Recipe-Component

--- client/src/pages/Recipedia.js
+++ client/src/pages/Recipedia.js
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "../components/Navbar/index";
 import Searchbar from "../components/Searchbar/index";
 
-function Search() {
-  return (
-    <>
-      <Navbar />
-      <Searchbar />
-    </>
-  );
+class Recipedia extends React.Component {
+  state = {
+    recipes: []
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Searchbar />
+      </>
+    );
+  }
 }
 
-export default Search;
+export default Recipedia;
