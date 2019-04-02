6f951ac9f1d0c9c932f569d562714ca0c0fd86d5
siaustin <sara.austin16@gmail.com>
2019-04-01 21:58:01 -0400

    recipe-card

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -6,62 +6,91 @@ function Recipe(props) {
   console.log(props);
 
   return (
-    <>
+    <Paper elevation={1} className="recipePage">
       {props.meal.label ? (
-        <>
-          <Paper>
-            <header />
-            <h1>
-              Meal:<span>{props.meal.label}</span>
-            </h1>
+        <div>
+          <div className="header">
             <img
               className="img"
               src={props.meal.image}
               alt={props.meal.label}
             />
-            <a href={props.meal.url} target="_blank">
-              Link to the recipe website
-            </a>
+            <h1>
+              Meal: <span>{props.meal.label}</span>
+            </h1>
+          </div>
+          <div className="body-card">
+            <div>
+              <h5>
+                <i class="fas fa-utensils" />
+                Calories:
+                <p className="circle">
+                  {parseFloat(props.meal.calories).toFixed(0)} cal
+                </p>
+              </h5>
+            </div>
+            <div>
+              <h5>
+                {" "}
+                <i class="fas fa-info-circle" />
+                Nutrition information:
+              </h5>
+              <ul>
+                <li>
+                  Fat:
+                  <span className="circle">
+                    {parseFloat(props.meal.digest[0].total).toFixed(0)} g
+                  </span>
+                </li>
+                <li>
+                  Carbohydrate:
+                  <span className="circle">
+                    {parseFloat(props.meal.digest[1].total).toFixed(0)} g
+                  </span>
+                </li>
+                <li>
+                  Protein:
+                  <span className="circle">
+                    {parseFloat(props.meal.digest[2].total).toFixed(0)} g
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h5>
+                <i class="far fa-clock" />
+                Prep time:
+                <span> {props.meal.time} min</span>
+              </h5>
+            </div>
+            <div>
+              <h5>
+                <i class="far fa-list-alt" />
+                Ingredients:
+              </h5>
+              <ul>
+                {props.meal.ingredients.map((item, i) => {
+                  return (
+                    <li key={i}>
+                      <i class="far fa-circle" />
+                      {item.text}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
             <h5>
-              Prep time:<span>{props.meal.time} min</span>
+              <a href={props.meal.url} target="_blank">
+                <i class="fas fa-external-link-alt" />
+                Link to the Recipe Website
+              </a>
             </h5>
-            <h5>
-              Calories:
-              <span>{parseFloat(props.meal.calories).toFixed(0)} cal</span>
-            </h5>
-            <h5>Nutrition information:</h5>
-            <ul>
-              <li>
-                Fat:
-                <span>
-                  {parseFloat(props.meal.digest[0].total).toFixed(0)} g
-                </span>
-              </li>
-              <li>
-                Carbohydrate:
-                <span>
-                  {parseFloat(props.meal.digest[1].total).toFixed(0)} g
-                </span>
-              </li>
-              <li>
-                Protein:
-                <span>
-                  {parseFloat(props.meal.digest[2].total).toFixed(0)} g
-                </span>
-              </li>
-            </ul>
-            <h5>Ingredients:</h5>
-            <ul>
-              {props.meal.ingredients.map((item, i) => {
-                return <li key={i}>{item.text}</li>;
-              })}
-            </ul>
-          </Paper>
-        </>
+          </div>
+        </div>
       ) : (
         ""
       )}
-    </>
+    </Paper>
   );
 }
 
