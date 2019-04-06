dd45e9d9e783c50e481260fc27530532a3ffb947
LBDCode <libby.duggan@gmail.com>
2019-04-06 17:04:29 -0400

    new page

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import "./style.css";
 import API from "../../utils/API";
 import Firebase from "../../config/Firebase";
-import { DragSource } from 'react-dnd'
+// import { DragSource } from 'react-dnd'
 // import Typography from '@material-ui/core/Typography';
 
 
@@ -19,26 +19,26 @@ export default class Carousel extends Component {
     currentUser: ""
   };
 
-  componentDidMount() {
-    var user = Firebase.auth().currentUser;
-    if (user) {
-      this.setState({
-        currentUser: user.email
-      });
-    }
-    this.getAll(user.email);
-  };
+  // componentDidMount() {
+  //   var user = Firebase.auth().currentUser;
+  //   if (user) {
+  //     this.setState({
+  //       currentUser: user.email
+  //     });
+  //   }
+  //   this.getAll(user.email);
+  // };
 
-  getAll(user) {
-    API.getDBRecipes(user)
-      .then(res => {
-        this.setState({
-          favorites: res.data.favorites
-        });
-        console.log(this.state.favorites);
-      })
-      .catch(err => console.log(err));
-  };
+  // getAll(user) {
+  //   API.getDBRecipes(user)
+  //     .then(res => {
+  //       this.setState({
+  //         favorites: res.data.favorites
+  //       });
+  //       console.log(this.state.favorites);
+  //     })
+  //     .catch(err => console.log(err));
+  // };
 
 
   
@@ -50,41 +50,15 @@ export default class Carousel extends Component {
       padding: '0.5rem',
       margin: '0.5rem',
     };
-    const SourceBoxRaw = ({
-      children,
-      isDragging,
-      connectDragSource
-    }) => {
-      const opacity = isDragging ? 0.4 : 1
-      
-      return connectDragSource(
-        <div
-          style={Object.assign({}, style, {
-            opacity
-          })}
-        >
-          {children}
-        </div>,
-      )
-    };
-    const SourceBox = DragSource(
-      props => props.color + '',
-      {
-        beginDrag: () => ({}),
-      },
-      (connect, monitor) => ({
-        connectDragSource: connect.dragSource(),
-        isDragging: monitor.isDragging(),
-      }),
-    )(SourceBoxRaw);
 
-    const StatefulSourceBox = props => {
+
+    const RecipeBox = props => {
       return (
-        <SourceBox
+        <div
           {...props}
           >
         <img src={props.image} data-obj={props.dataObj} alt="recipe"/>
-        </ SourceBox>
+        </ div>
       )
     };
     
@@ -132,9 +106,9 @@ export default class Carousel extends Component {
     return (
       <div>
         <Slider {...settings}>
-          {this.state.favorites.map(item => {
+          {this.props.favorites.map(item => {
             return (
-              <StatefulSourceBox
+              <RecipeBox
               image={item.image}
               dataObj={item}
             />
